bc69813ed9f4d316779c3440412c90c9bd509e5c
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-10-17 10:22:48 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -10,16 +10,3 @@ alert('太郎が来た');
 function hanako66(){
 alert('花子が来た');
 }
-
-var who67 ='world';
-function sayhello67(){
-alert('Hello, ' + who67 + '!');
-}
-
-function taro67(){
-  who67='太郎';
-}
-
-function hanako67(){
-  who67='花子';
-}
