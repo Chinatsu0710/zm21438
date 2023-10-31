01a8bfa191d6c483246943e2e18abd88c9b0f935
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-10-31 09:42:53 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,9 @@
+function sayhello68(){
+  alert('Hello,'+ document.getElementByld('who68').innerHTML + '!');
+}
+    function taro68(){
+  alert(document.getElementByld('who68').innerHTML + '太郎');
+    }
+function hanoko68(){
+  alert(document.getElementByld('who68').innerHTML + '花子');
+}
