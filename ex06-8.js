d605209a89c9551e646b545d7edf124bb3168bea
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-11-14 10:34:25 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,4 +1,4 @@
 // function sayhello68(){alert('Hello,'+ document.getElementById('who68').innerHTML + '!');}
-function sayhello68(){'Hello,';}
+function sayhello68(){alert('Hello,');}
 function taro68(){document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){document.getElementById('who68').innerHTML='花子';}
