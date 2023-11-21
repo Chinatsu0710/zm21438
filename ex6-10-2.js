c63702f13b207a4d0a17977330e423c0261bf483
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-11-21 10:29:56 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,2 +1,2 @@
-function imgchange3(){ document.getElementById('logo').src='./image/julogo.gif';}
-function imgchange4(){ document.getElementById('logo').src='./image/josai1.gif';}
+function imgchange3(){ document.getElementById('ju').src='./image/julogo.gif';}
+function imgchange4(){ document.getElementById('ju').src='./image/jokunn.gif';}
