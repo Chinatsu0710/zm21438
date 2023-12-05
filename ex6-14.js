8b338b6e19859fb077a41af514add610abe485d1
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-12-05 10:39:11 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,3 @@
+var img;
+function imagechange5(){document.getElementById('').src='./image/tree_fuyu.png'; setTimeout("imagechange6()",3000);}
+function imagechange6(){document.getElementById('').src='./image/snowman_yukidaruma_man';}
