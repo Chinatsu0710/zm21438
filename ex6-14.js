066de252ae8fbdd2d45945602c136cd7c2751ddf
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-12-05 10:41:13 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
 var img;
-function imagechange5(){document.getElementById('').src='./image/tree_fuyu.png'; setTimeout("imagechange6()",3000);}
-function imagechange6(){document.getElementById('').src='./image/snowman_yukidaruma_man';}
+function imagechange5(){document.getElementById('fuyu').src='./image/tree_fuyu.png'; setTimeout("imagechange6()",3000);}
+function imagechange6(){document.getElementById('fuyu').src='./image/snowman_yukidaruma_man';}
