3727d1a7b5ee73a9cded64b3cf09025b7506fc9f
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-12-12 09:39:09 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,2 +1,2 @@
 function page(){document.getElementById('1').src ='./image/snowman.png';}
-function page2(){document.getElementById('1').src ='./image/fuyu.png';}
+function page2(){document.getElementById('1').src ='./image/tree_fuyu.png';}
