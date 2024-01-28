3b73ab1a6c0c39f927a3ea4f031acfaa35dba97c
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2024-01-29 01:23:00 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,4 +1,14 @@
 // function sayhello68(){alert('Hello,'+ document.getElementById('who68').innerHTML + '!');}
-function sayhello68(){alert('Hello,');}
-function taro68(){document.getElementById('who68').innerHTML='太郎';}
-function hanako68(){document.getElementById('who68').innerHTML='花子';}
+//function sayhello68(){alert('Hello,');}
+//function taro68(){document.getElementById('who68').innerHTML='太郎';}
+//function hanako68(){document.getElementById('who68').innerHTML='花子';}
+function sayhello68() {
+  alert('Hello,' + document.getElementById('who68').innerHTML + '!');
+}
+function taro68() {
+  document.getElementById('who68').innerHTML= '太郎';
+}
+function hanako68() {
+  document.getElementById('who68').innerHTML='花子';
+}
+                    
